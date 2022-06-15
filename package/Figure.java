a23d251c7a8a392c039547b35c757e994c409cc4
Abhirami0203 <97434171+Abhirami0203@users.noreply.github.com>
2022-06-15 11:44:00 +0530

    Add files via upload

---
+++ package/Figure.java
@@ -0,0 +1,14 @@
+import Graphics.*;
+import java.io.*;
+class Figure
+{
+public static void main(String[] args)
+{
+Rectangle1 r=new Rectangle1();
+r.getdata();
+r.area();
+Student1 s=new Student1();
+s.get();
+s.show();
+}
+}
---
+++ package/Graphics/Rectangle1.java
@@ -0,0 +1,18 @@
+package Graphics;
+import java.io.*;
+import java.util.*;
+public class Rectangle1
+{
+int l,b;
+Scanner sc=new Scanner(System.in);
+public void getdata()
+{
+System.out.println("Enter length and breadth");
+l=sc.nextInt();
+b=sc.nextInt();
+}
+public void area()
+{
+System.out.println("Area of rectangle;"+(l*b));
+}
+}
